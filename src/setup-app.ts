2d4b0109e130282b0a114a720140c6f2ddf9cd43
Margo <margokomilfo.dev@gmail.com>
2025-03-26 11:44:57 +0100

    base crud

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,10 +1,53 @@
 import express, { Express, Request, Response } from 'express';
+import { db } from './db/in-memory.db';
+import { HttpStatus } from './core/types/http-statuses';
+import { Driver, DriverStatus } from './drivers/types/driver';
 
 export const setupApp = (app: Express) => {
-  app.use(express.json()); //middleware express.json() парсит JSON в теле запроса и добавляет его как объект в свойство body запроса (req.body.).
+  app.use(express.json());
 
   app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('Hello world!');
+    res.status(200).send('hello world!!!');
+  });
+
+  app.get('/drivers', (req: Request, res: Response) => {
+    res.status(200).send(db.drivers);
+  });
+  app.get('/drivers/:id', (req: Request, res: Response) => {
+    const driver = db.drivers.find((d) => d.id === +req.params.id);
+    res.status(200).send(driver);
+  });
+  app.post('/drivers', (req: Request, res: Response) => {
+    //1) проверяем приходящие данные на валидность
+
+    //2) создаем newDriver
+    const newDriver: Driver = {
+      id: db.drivers.length ? db.drivers[db.drivers.length - 1].id + 1 : 1,
+      name: req.body.name,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+      status: DriverStatus.Online,
+      vehicleMake: req.body.vehicleMake,
+      vehicleModel: req.body.vehicleModel,
+      vehicleYear: req.body.vehicleYear,
+      vehicleLicensePlate: req.body.vehicleLicensePlate,
+      vehicleDescription: req.body.vehicleDescription,
+      vehicleFeatures: req.body.vehicleFeatures,
+      createdAt: new Date(),
+    };
+
+    //3) добавляем newDriver в БД
+    db.drivers.push(newDriver);
+    res.status(HttpStatus.Created).send(newDriver);
+  });
+
+  app.get('/testing', (req: Request, res: Response) => {
+    res.status(200).send('testing url');
+  });
+
+  app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.drivers = [];
+    res.sendStatus(HttpStatus.NoContent);
   });
 
   return app;
